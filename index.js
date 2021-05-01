b7bc4a8563ecd8985db4c8fb30f7a802d747782a
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-05-01 21:55:53 +0000

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const email = document.getElementById('mail');
-const password = document.getElementById("password");
+let password = document.getElementById("password");
+let hidepwtxt = document.getElementById("hidepwtxt");
 function redirect(){
   window.location = "/profile.html?email="+ email.value;
+}
+function hidepw(){
+  let boolean = password.type == 'password';
+  password.type = boolean ? 'text' : 'password';
+  hidepwtxt.innerHTML = boolean ? 'Hide Password' : 'Show Password';
 }
